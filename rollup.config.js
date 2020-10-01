2b34022035cd08a5cfaa00095caac04a1dec9b03
Jim Fisher <jameshfisher@gmail.com>
2020-10-01 14:33:02 +0100

    "Fix" the problem with gross plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,12 @@
 import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 export default {
   input: 'main.es.js',
   output: { file: 'bundle.js', format: 'iife' },
-  plugins: [ resolve() ]
+  plugins: [ 
+    commonjs(), 
+    nodePolyfills(),
+    resolve()
+  ]
 };
